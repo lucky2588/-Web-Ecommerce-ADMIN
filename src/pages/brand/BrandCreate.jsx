eed55bbf4bcc60210ef6a7ce3a916a189f031a9d
lucky2588 <duwcsthang2001@gmail.com>
2023-07-06 02:30:10 +0700

    Create BrandCreate.jsx

---
+++ src/pages/brand/BrandCreate.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
+function BrandCreate() {
+  const natigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = async (obj) => {
+    const objPush = {
+      name: obj.name,
+      description: obj.description,
+      thumbail: obj.thumbail
+    }
+    try {
+      const response = await axios.post(`http://localhost:8888/api/v1/admin/craeteBrand`, objPush);
+      toast.success("Create brand secesss !! ")
+      natigate("/admin/brand")
+    } catch (err) {
+      alert(err);
+    }
+  }
+  return (
+    <>
+      <div className='main'>
+        <div className="page-heading">
+          <div className="page-title">
+            <div className="row">
+              <div className="col-12 col-md-6 order-md-1 order-last">
+                <Link className="btn btn-warning waves-effect waves-light m-3">Back</Link>
+                <h3>Create Brand</h3>
+                <p className="text-subtitle text-muted"> Infomation about Brand </p>
+              </div>
+
+            </div>
+          </div>
+          <form method='Post' onSubmit={handleSubmit(onSubmit)}>
+            <section className="section">
+              <div className="row">
+                <div className="col">
+                  <div className="card">
+                    <div className="card-header">
+                      Name 
+                    </div>
+                    <div className="card-body">
+                      <div className="col-sm-10">
+                        <input type="text" className="form-control"
+                          id="basic-default-name"
+                          {...register("name",
+                            {
+                              required: true
+                            }
+                          )
+                          }
+                          />
+                        {Object.keys(errors).length !== 0 && (
+                          <ul>
+                            {errors.name?.type === "required" &&
+                              <li className='text-danger'>This field is not empty</li>
+                            }
+                          </ul>
+                        )
+                        }
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <div className="card-header">
+                      Thumbail
+                    </div>
+                    <div className="card-body">
+                      <div className="col-sm-10">
+                        <input type="text" className="form-control"
+                          id="basic-default-name"
+                          {...register("thumbail",
+                            {
+                              required: true
+                            }
+                          )
+                          }
+                        />
+                        {Object.keys(errors).length !== 0 && (
+                          <ul>
+                            {errors.thumbail?.type === "required" &&
+                              <li className='text-danger'>This field is not empty</li>
+                            }
+                          </ul>
+                        )
+                        }
+                      </div>
+                    </div>
+                  </div>
+
+
+
+                  <div className="card">
+                    <div className="card-header">
+                      Description
+                    </div>
+                    <div className="card-body">
+                      <div className="form-group mb-3">
+                       
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""}
+                          {...register("description",
+                            {
+                              required: true
+                            }
+                          )
+                          }
+                        />
+                        {Object.keys(errors).length !== 0 && (
+                          <ul>
+                            {errors.description?.type === "required" &&
+                              <li className='text-danger'>This field is not empty</li>
+                            }
+                          </ul>
+                        )
+                        }
+                      </div>
+                    </div>
+                  </div>
+
+
+
+                </div>
+              </div>
+            </section>
+            <div className="text-center">
+              <button type="submit" className="btn btn-success waves-effect waves-light mx-3">Create</button>
+
+            </div>
+          </form>
+        </div>
+
+
+
+      </div>
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default BrandCreate
