911be0425e94386e7cb220ff0bb37fd168c7869f
lucky2588 <duwcsthang2001@gmail.com>
2023-07-06 02:31:04 +0700

    Create store.js

---
+++ src/app/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "./slice/authSlice";
+import { authApi } from "./service/authApi";
+import { blogApi } from "./service/blogApi";
+import { productApi } from "./service/productApi";
+import { categoryApi } from "./service/categoryApi";
+import { brandApi } from "./service/brandApi";
+import { userApi } from "./service/userApi";
+import { orderApi } from "./service/orderApi";
+import { infoApi } from "./service/infoApi";
+
+
+
+export const store = configureStore(
+    {
+        reducer: {
+            [brandApi.reducerPath]: brandApi.reducer,
+            [infoApi.reducerPath]: infoApi.reducer,
+            [orderApi.reducerPath]: orderApi.reducer,
+            [categoryApi.reducerPath]: categoryApi.reducer,
+            [userApi.reducerPath]: userApi.reducer,
+            [productApi.reducerPath]: productApi.reducer,
+            [blogApi.reducerPath]: blogApi.reducer,
+            [authApi.reducerPath]: authApi.reducer,
+            auth: authReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(authApi.middleware, infoApi.middleware, userApi.middleware, brandApi.middleware, orderApi.middleware, blogApi.middleware, productApi.middleware, categoryApi.middleware)
+    },
+
+)
