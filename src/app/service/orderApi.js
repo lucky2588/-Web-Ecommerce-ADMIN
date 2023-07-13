fd74ef72f7937b1502a0f13d1ad4b78314b72e5e
lucky2588 <duwcsthang2001@gmail.com>
2023-07-14 01:37:07 +0700

    Update orderApi.js

--- src/app/service/orderApi.js
+++ src/app/service/orderApi.js
@@ -4,29 +4,28 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseUrl = "http://localhost:8888/api/v1/admin"
 export const orderApi = createApi({
     reducerPath: "orderApi",
-    baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token;
-
-        if (token) {
-            headers.set("Authorization", `Bearer ${token}`);
-        }
-        return headers;
-    },
+    baseQuery: fetchBaseQuery({ baseUrl: baseUrl ,
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().auth.token;
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+  
     endpoints: (builder) => ({
-        getMyOrder: builder.query({
-            query: (email) => `myOrder/${email}`
-        }),
         getPayment: builder.query({
             query: (paymentId) => `getPayment/${paymentId}`
         }),
+        
         getBillbyId: builder.query({
             query: (orderId) => `getOrder/${orderId}`
         }),
         getMyBill: builder.query({ // use to get Bill
             query: (email) => `getMyBill/${email}`
         }),
-        getOrderToday: builder.query({ // use to get Bill
+        getOrderToday: builder.query({ 
             query: () =>  `getOrderToday`
         }),
         
@@ -36,4 +35,4 @@ export const orderApi = createApi({
 
 });
 
-export const {useGetMyOrderQuery,useLazyGetMyOrderQuery,useGetProductSimilarQuery,useGetBillbyIdQuery,useGetMyBillQuery,useGetPaymentQuery,useGetOrderTodayQuery} = orderApi;
+export const {useGetBillbyIdQuery,useGetMyBillQuery,useGetPaymentQuery,useGetOrderTodayQuery} = orderApi;
