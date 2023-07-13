f266924c5f30cfc2f106c7dcf325788c01fb174c
lucky2588 <duwcsthang2001@gmail.com>
2023-07-14 01:36:26 +0700

    Create CommentsBlog.jsx

---
+++ src/pages/comments/CommentsBlog.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+function CommentsBlog() {
+    return (
+        <>
+            <main className="main">
+                <section class="section">
+
+                    <div class="row">
+                        <div className="col-lg-12">
+                            <div class="card mb-3">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="https://banner2.cleanpng.com/20180402/xze/kisspng-user-avatar-computer-icons-blog-user-avatar-5ac207cc923637.5022522215226654205989.jpg" class="img-fluid rounded-start" alt="..." />
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card with an image on left</h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">With Heading &amp; Separator</h5>
+                                    <div className="alert alert-primary alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Primary Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-secondary alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Secondary Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-success alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Success Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Danger Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-warning  alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Warning Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-info  alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Info Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-light border-light alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Lignt Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                    <div className="alert alert-dark  alert-dismissible fade show" role="alert">
+                                        <h4 className="alert-heading">Dark Heading</h4>
+                                        <p>Et suscipit deserunt earum itaque dignissimos recusandae dolorem qui. Molestiae rerum perferendis laborum. Occaecati illo at laboriosam rem molestiae sint.</p>
+                                        <hr />
+                                        <p className="mb-0">Temporibus quis et qui aspernatur laboriosam sit eveniet qui sunt.</p>
+                                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </main>
+
+
+
+
+
+
+
+
+
+
+        </>
+    )
+}
+
+export default CommentsBlog
